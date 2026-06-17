804e76e0e900ae8b54e13c116de7e13c279d8aee
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-18 03:48:12 +0500

    feat(territories): app config

---
+++ apps/territories/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TerritoriesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.territories"
+    verbose_name = "Territory & Quota Management"
