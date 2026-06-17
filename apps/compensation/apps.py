f989c4d4776dbb68014a31ec2e117c8c0436d469
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-18 03:48:50 +0500

    feat(compensation): app config

---
+++ apps/compensation/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CompensationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.compensation"
+    verbose_name = "Incentive Compensation Management"
