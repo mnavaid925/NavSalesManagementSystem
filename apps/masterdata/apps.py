03a1770da47b46600cc51be218ef5d02021d9cb1
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-19 15:40:44 +0500

    feat(masterdata): app config

---
+++ apps/masterdata/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MasterdataConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.masterdata"
+    verbose_name = "Master Data & Configuration"
